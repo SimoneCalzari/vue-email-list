5d27f1f414e9068af3db0f70099ffb514e4b0889
SimoneCalzari <simone.calzari@outlook.it>
2023-12-05 17:19:19 +0100

    feat: esercizio + bonus tranne estetica css

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 "use strict";
 
-console.log("ciao");
+const { createApp } = Vue;
+
+createApp({
+  data() {
+    return {
+      emailList: [],
+      apiBoolean: "https://flynn.boolean.careers/exercises/api/random/mail",
+      numEmail: 10,
+    };
+  },
+  mounted() {
+    for (let i = 0; i < this.numEmail; i++) {
+      axios.get(this.apiBoolean).then((emailResponse) => {
+        console.log("cpu");
+        this.emailList.push(emailResponse.data.response);
+      });
+    }
+  },
+  methods: {},
+}).mount("#app");
